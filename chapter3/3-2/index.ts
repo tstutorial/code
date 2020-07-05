31cc79f6602b04465ff8e1e5113f6b016edcd3f0
szhong <szhong@beckman.com>
2020-07-05 10:52:48 +0800

    chapter3

---
+++ chapter3/3-2/index.ts
@@ -0,0 +1,17 @@
+// single line comment
+const a = 0;
+
+/** multi-line comment */
+const b = 0;
+
+/**
+ * multi-line comment
+ * multi-line comment
+ */
+const c = 0;
+
+//#region 区域描述
+
+let d = 0;
+
+//#endregion
